22aad1f1abe5d56f0794c781eae56eed85f8801d
NvsYashwanth <nvsyashwanth338@gmail.com>
2020-07-29 17:11:59 +0530

    Palindromes

--- Valid Palindrome II.py
+++ Valid Palindrome II.py
@@ -1,10 +1,42 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        def is_pali_range(i, j):
-            return all(s[k] == s[j-k+i] for k in range(i, j))
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        """
+        Given an input string s, this program determines whether s is a
+        valid palindrome or becomes one with the removal of a single
+        character.
 
-        for i in xrange(len(s) / 2):
-            if s[i] != s[~i]:
-                j = len(s) - 1 - i
-                return is_pali_range(i+1, j) or is_pali_range(i, j-1)
-        return True
+        :param s: input string
+        :type s: str
+        :return: True if s is palindrome unmodified or with the removal
+                 of a single character, else False
+        :rtype: bool
+        """
+        """
+        If s is a palindrome unmodified, return True right away.
+        """
+        if s == s[::-1]:
+            return True
+
+        """
+        The string s may be a palindrome with the removal of a character.
+        The while loop finds two possible locations of the offending
+        character, s[left] (leftmost) and s[right] (rightmost).
+        """
+        left = 0
+        right = len( s ) - 1
+        while s[left] == s[right]:
+            left += 1
+            right -= 1
+
+        """
+        Check whether substring created by removing either the leftmost
+        character or rightmost character from the while loop is a valid
+        palindrome.
+        """
+        new_s = s[left+1:right+1]
+        if new_s == new_s[::-1]:
+            return True
+        new_s = s[left:right]
+        if new_s == new_s[::-1]:
+            return True
+        return False
